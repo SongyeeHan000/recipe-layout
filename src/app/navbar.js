aca7dd4a9cfca55fd42437be2f07d30798aaeaea
SongyeeHan000 <hansongyee0304@gmail.com>
2024-03-30 16:31:11 -0400

    Navbar is linked

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -1,13 +1,13 @@
-import "../../public/globals.css";
-import { Link } from "react-router-dom";
+import "./globals.css";
+import Link from "next/link";
 
 export default function Navbar() {
     return ( 
         <div className="navBar">
+            <Link href="/">Home</Link>
+            <Link href="/recipes">Recipes</Link>
+            <Link href="/contact">Contact</Link>
             
-            <h1>Home</h1>
-            <h1>Recipes</h1>
-            <h1>Contact</h1>
         </div>
     )
 }
