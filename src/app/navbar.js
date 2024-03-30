ac2da3097cfeb7b2d9b391f271e3647831f9bbc5
SongyeeHan000 <hansongyee0304@gmail.com>
2024-03-30 13:54:36 -0400

    installed react dom to add routers, added template design from frontend mentor, and added moved globalcss to public

---
+++ src/app/navbar.js
@@ -0,0 +1,13 @@
+import "../../public/globals.css";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+    return ( 
+        <div className="navBar">
+            
+            <h1>Home</h1>
+            <h1>Recipes</h1>
+            <h1>Contact</h1>
+        </div>
+    )
+}
