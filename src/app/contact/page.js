26e148882e59524e5ab516c695db39c22cb1cb30
SongyeeHan000 <hansongyee0304@gmail.com>
2024-04-01 21:31:46 -0400

    contact format completed for desktop

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,7 +1,43 @@
 'use client'
 
-export default function Contact () {
+// export default function Contact () {
+//   return (
+//     <h1>Contact Page </h1>
+//   );
+// }
+
+
+import React, { useState } from "react"
+
+export default function Contact() {
   return (
-    <h1>Contact Page </h1>
-  );
+    <div className="contactForm">
+        <div className="getInTouch"> 
+            <h1>Get in Touch!</h1>
+            <p>Proin volutpat consequat porttitor cras nullam gravida at. Orci molestie a eu arcu. Sed ut tincidunt integer elementum id sem. Arcu sed malesuada et magna.</p>
+        </div>
+     
+        <form>
+            <div>
+            <label>First Name</label>
+                <input type="text" name="firstName" />
+            </div>
+            <div>
+            <label>Last Name</label>
+                <input type="text" name="lastName" />
+            </div>
+
+            <div>
+                <label>Email</label>
+                <input type="text" name="email" />
+            </div>
+
+            <div>
+                <label>Message</label>
+                <textarea name="message"></textarea>
+            </div>
+            <button type="submit">Send message</button>
+        </form>
+    </div>
+  )
 }
